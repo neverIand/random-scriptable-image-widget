a0054a2053d904f8b77af786357bbda0642587e9
Ricky Tang <rickytang2019@gmail.com>
2024-05-10 01:32:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const SCRIPT_CONFIG = {
+  title: "我去，",
+  month: 5,
+  date: 13,
+  msg: "生日快乐，蔡宝",
+  imgKeywords: ["vocaloid", "miku"].join(","),
+  bDayImgKeywords: ["cake", "round"].join(","),
+};
+
 let widget = await createWidget();
 if (config.runsInWidget) {
   Script.setWidget(widget);
@@ -8,7 +17,6 @@ Script.complete();
 
 async function createWidget() {
   let img = await loadRandomVocaloidImage();
-  let title = "我去，";
   let widget = new ListWidget();
 
   // let gradient = new LinearGradient();
@@ -18,7 +26,7 @@ async function createWidget() {
   widget.backgroundImage = img;
 
   let titleStack = widget.addStack();
-  let titleElement = titleStack.addText(title);
+  let titleElement = titleStack.addText(SCRIPT_CONFIG.title);
   titleElement.textColor = Color.white();
   titleElement.textOpacity = 0.95;
   titleElement.font = Font.boldSystemFont(24);
@@ -26,7 +34,7 @@ async function createWidget() {
   widget.addSpacer(4);
 
   if (isBDay()) {
-    let textElement = widget.addText("生日快乐，蔡宝");
+    let textElement = widget.addText(SCRIPT_CONFIG.msg);
     textElement.textColor = Color.white();
     textElement.textOpacity = 0.9;
     textElement.font = Font.boldSystemFont(20);
@@ -38,8 +46,8 @@ async function createWidget() {
 async function loadImgMetaData() {
   // see loremflickr docs
   let url = isBDay()
-    ? "https://loremflickr.com/json/g/540/540/cake,round/all?random=1"
-    : "https://loremflickr.com/json/g/540/540/vocaloid,miku/all?random=1";
+    ? `https://loremflickr.com/json/g/540/540/${SCRIPT_CONFIG.bDayImgKeywords}/all?random=1`
+    : `https://loremflickr.com/json/g/540/540/${SCRIPT_CONFIG.imgKeywords}/all?random=1`;
   let req = new Request(url);
   return await req.loadJSON();
 }
@@ -53,5 +61,8 @@ async function loadRandomVocaloidImage() {
 
 function isBDay() {
   const date = new Date();
-  return date.getMonth() === 4 && date.getDate() === 13;
+  return (
+    date.getMonth() === SCRIPT_CONFIG.month - 1 &&
+    date.getDate() === SCRIPT_CONFIG.date
+  );
 }
